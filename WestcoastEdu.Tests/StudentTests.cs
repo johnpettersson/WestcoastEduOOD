578cd60b6e0701e53760d96d3dfa199cc95e0de7
John Pettersson <john.pettersson.vgr@gmail.com>
2022-12-14 18:31:32 +0100

    added some tests

---
+++ WestcoastEdu.Tests/StudentTests.cs
@@ -0,0 +1,33 @@
+using WestCoastEdu.BCL.People;
+using WestCoastEdu.BCL.Courses;
+
+namespace WestcoastEdu.Tests;
+
+public class StudentTests
+{
+    [Fact]
+    public void Test_StudentJoinCourse_ThrowsException_IfNotAuthenticated()
+    {
+        // arrenge
+        Student student = new Student();
+        //student.Authenticate(""); //authenticate never called = user not authenticated
+
+        // act
+        Action act = () => student.JoinCourse(new CourseOccasion());
+
+        // assert
+        Assert.Throws<Exception>(act);
+    }
+
+    [Fact]
+    public void Test_StudentNameNullOnCreation()
+    {
+        //arrenge
+        //act
+        Student student = new Student();
+
+        //assert
+        Assert.Null(student.FirstName);
+        Assert.Null(student.LastName);
+    }
+}
