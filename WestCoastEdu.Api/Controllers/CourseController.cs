cb222d4b29e89c42a5d2a05a49b0189da38784ca
John Pettersson <john.pettersson.vgr@gmail.com>
2023-01-23 11:24:54 +0100

    CourseController, first endpoints

--- WestCoastEdu.Api/Controllers/CourseController.cs
+++ WestCoastEdu.Api/Controllers/CourseController.cs
@@ -10,21 +10,59 @@ namespace WestcoastEdu.Api.Controllers;
 public class CourseController : ControllerBase
 {
 
-    [HttpGet("[action]")]
+    [HttpGet("list")]
     public ActionResult List()
     {
         return StatusCode(200, new { message = "Hello World!" });
     }
 
-    [HttpGet("[action]/{id}")]
+    [HttpGet("id/{id}")]
     public ActionResult GetById(int id)
     {
-        return StatusCode(200, new { message = "GetById fungerar", id = id });
+        return StatusCode(200, new { message = "GetById fungerar", id});
     }
 
-    [HttpGet("[action]/{name}")]
-    public ActionResult GetByName(string name)
+    [HttpGet("number/{courseNumber}")]
+    public ActionResult GetByCourseNumber(string courseNumber)
     {
-        return StatusCode(200, new { message = "GetByName fungerar", name = name});
+        return StatusCode(200, new { message = "GetByCourseNumber fungerar", courseNumber});
+    }   
+
+    [HttpGet("title/{title}")]
+    public ActionResult GetByCourseTitle(string title)
+    {
+        return StatusCode(200, new { message = "GetByCourseTitle fungerar", title});
     }
+
+    [HttpGet("startdate/{date}")]
+    public ActionResult GetByStartDate(DateTime date)
+    {
+        return StatusCode(200, new { message = "GetByStartDate fungerar", date});
+    }
+
+    [HttpPost]
+    public ActionResult CreateCourse()
+    {
+        return Created("uri123", new { message = "CreateCourse fungerar"});
+    }
+
+    [HttpPut]
+    public ActionResult UpdateCourse()
+    {
+        return NoContent();
+    }
+
+    [HttpPatch("markfullybooked/{courseId}")]
+    public ActionResult MarkCourseFullyBooked(int courseId)
+    {
+        return NoContent();
+    }
+
+
+    [HttpPatch("markcomplete/{courseId}")]
+    public ActionResult MarkCourseCompleted(int courseId)
+    {
+        return NoContent();
+    }
+
 }
