2c9c35e6a69139fed81d7d5bd120818cdb8c3781
John Pettersson <john.pettersson.vgr@gmail.com>
2023-01-24 18:31:13 +0100

    CourseController finished

--- WestCoastEdu.Api/Controllers/CourseController.cs
+++ WestCoastEdu.Api/Controllers/CourseController.cs
@@ -13,55 +13,64 @@ public class CourseController : ControllerBase
     [HttpGet("list")]
     public ActionResult List()
     {
+        //TODO: Hämta alla kurser
         return StatusCode(200, new { message = "Hello World!" });
     }
 
-    [HttpGet("id/{id}")]
+    [HttpGet("{id}")]
     public ActionResult GetById(int id)
     {
+        //TODO: HÄmta kursen som matchar idt
         return StatusCode(200, new { message = "GetById fungerar", id});
     }
 
     [HttpGet("number/{courseNumber}")]
     public ActionResult GetByCourseNumber(string courseNumber)
     {
+        //TODO: Hämta kursen som matchar kursnumret.
         return StatusCode(200, new { message = "GetByCourseNumber fungerar", courseNumber});
     }   
 
     [HttpGet("title/{title}")]
     public ActionResult GetByCourseTitle(string title)
     {
+        //TODO: Hämta kursen som matchar titeln 
         return StatusCode(200, new { message = "GetByCourseTitle fungerar", title});
     }
 
     [HttpGet("startdate/{date}")]
     public ActionResult GetByStartDate(DateTime date)
     {
+        //TODO: Hämta kurser som matchar datumet 
         return StatusCode(200, new { message = "GetByStartDate fungerar", date});
     }
 
     [HttpPost]
-    public ActionResult CreateCourse()
+    public ActionResult CreateCourse(object model)
     {
+        //TODO: Skapa en kurs och spara den 
         return Created("uri123", new { message = "CreateCourse fungerar"});
     }
 
-    [HttpPut]
-    public ActionResult UpdateCourse()
+    [HttpPut("{id}")]
+    public ActionResult UpdateCourse(int id, object model)
     {
+        //TODO: Uppdatera kursen 
         return NoContent();
     }
 
-    [HttpPatch("markfullybooked/{courseId}")]
+    [HttpPatch("fullybooked/{courseId}")]
     public ActionResult MarkCourseFullyBooked(int courseId)
     {
+        //TODO: Updatera kursens isFullyBooked-fält 
         return NoContent();
     }
 
 
-    [HttpPatch("markcomplete/{courseId}")]
+    [HttpPatch("complete/{courseId}")]
     public ActionResult MarkCourseCompleted(int courseId)
     {
+        //TODO: Updatera kursens isCompleted-fält 
         return NoContent();
     }
 
